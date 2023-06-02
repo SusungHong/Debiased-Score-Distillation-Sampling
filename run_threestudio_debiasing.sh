05c77d5a384ba579f3f45a2d71aef8c76d91f0c0
PnDong <joseph1999@naver.com>
2023-06-02 20:03:53 +0900

    sh file

---
+++ run_threestudio_debiasing.sh
@@ -0,0 +1,2 @@
+# Sampling with score debiasing
+python launch.py --config configs/dreamfusion-if.yaml --train --gpu 0 system.prompt_processor.prompt="a colorful toucan with a large beak" system.guidance.grad_clip=[0,0.5,2.0,10000]
